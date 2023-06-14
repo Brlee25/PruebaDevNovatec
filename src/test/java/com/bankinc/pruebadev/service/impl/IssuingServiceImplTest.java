0111dbc119e8b44523f138d9a2949b60fedb578a
Brandon C <Brandon C@DESKTOP-7E471K9>
2023-06-14 10:55:10 -0500

    Ajustes de Pom.xml
    Se ajusta la versión de la dependencia jupiter-api, y adicionalmente se
    elimina la importación de una clase no ultilizada en la clase
    IssuingServiceImplTest.

--- src/test/java/com/bankinc/pruebadev/service/impl/IssuingServiceImplTest.java
+++ src/test/java/com/bankinc/pruebadev/service/impl/IssuingServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.bankinc.pruebadev.service.impl;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
